e2ea5afdee426a145f47f45a7461832a8e6484d6
Max Ludwick <max.ludwick@gmail.com>
2025-04-04 13:38:17 +0200

    correct url

--- FotoRec_client.py
+++ FotoRec_client.py
@@ -32,7 +32,7 @@ AWS_REGION = "eu-west-1"
 COLLECTION_ID = "eyespy-faces"  
 
 # Backend server configuration
-DEFAULT_BACKEND_URL = "http://15.236.226.31:8080"
+DEFAULT_BACKEND_URL = "http://35.180.226.30:8080"
 backend_url = os.environ.get("EYESPY_BACKEND_URL", DEFAULT_BACKEND_URL)
 
 # Initialize variables
